3466abe4afb336f6f2d1832ddb0c8ba319b540b5
Milton-Moses <miltonmoses05@gmail.com>
2025-09-14 18:36:37 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module farmingdale.collocate {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens farmingdale.collocate to javafx.fxml;
+    exports farmingdale.collocate;
+}
