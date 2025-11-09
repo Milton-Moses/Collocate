ccd408c06946a617a8e8c9dcd65078e00de30495
jabaltariq <21aidanrodriguez1@gmail.com>
2025-11-08 19:50:17 -0500

    Added very basic functionality to login / register pages - they currently will switch between each other regardless of if the pages contain valid inputs or not.

--- src/main/java/farmingdale/collocate/LoginController.java
+++ src/main/java/farmingdale/collocate/LoginController.java
@@ -1,4 +1,25 @@
 package farmingdale.collocate;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    private Button registerButton, loginButton;
+
+    @FXML
+    private TextField usernameField, passField;
+
+    @FXML
+    private void goToRegister() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("register-view.fxml"));
+        Main.scene = new Scene(fxmlLoader.load(), 1420, 1080);
+        Main.mainStage.setScene(Main.scene);
+        Main.mainStage.show();
+    }
 }
--- src/main/java/farmingdale/collocate/RegisterController.java
+++ src/main/java/farmingdale/collocate/RegisterController.java
@@ -1,4 +1,25 @@
 package farmingdale.collocate;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class RegisterController {
+    @FXML
+    private TextField fNameField, lNameField, orgField, passField, confirmPassField, emailField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private void goToLogin() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        Main.scene = new Scene(fxmlLoader.load(), 1420, 1080);
+        Main.mainStage.setScene(Main.scene);
+        Main.mainStage.show();
+    }
 }
