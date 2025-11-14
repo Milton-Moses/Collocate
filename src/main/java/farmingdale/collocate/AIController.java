2155e78aaf668fec5f6dc99b68ee14101c1ecf44
jabaltariq <21aidanrodriguez1@gmail.com>
2025-11-13 23:18:28 -0500

    Created base layout for ai-view.fxml and created basic functionality in AIController.java.

--- src/main/java/farmingdale/collocate/AIController.java
+++ src/main/java/farmingdale/collocate/AIController.java
@@ -1,5 +1,31 @@
 package farmingdale.collocate;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class AIController {
     private String promptText = "";
+
+    @FXML
+    private TextField promptField;
+
+    @FXML
+    private Button returnButton;
+
+    /**
+     * Returns the user to the Calendar page.
+     * @throws IOException Thrown if FXML file fails to load.
+     * @author Aidan Rodriguez
+     */
+    private void returnToCalendar() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("calendar-view.fxml"));
+        Main.scene = new Scene(fxmlLoader.load(), 1420, 1080);
+        Main.mainStage.setScene(Main.scene);
+        Main.mainStage.show();
+    }
 }
