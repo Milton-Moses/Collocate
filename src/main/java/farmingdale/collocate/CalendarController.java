1b0f81511d088608f4c7deb4369190f233f5ad02
jabaltariq <21aidanrodriguez1@gmail.com>
2025-11-13 21:50:17 -0500

    Updated calender-view.fxml to include buttons to navigate application, updated CalendarController.java to allow the user to return to the login page.

--- src/main/java/farmingdale/collocate/CalendarController.java
+++ src/main/java/farmingdale/collocate/CalendarController.java
@@ -1,4 +1,21 @@
 package farmingdale.collocate;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class CalendarController {
+    @FXML
+    private Button aiButton, logoutButton, clientButton, newEventButton;
+
+    @FXML
+    private void logOut() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        Main.scene = new Scene(fxmlLoader.load(), 1420, 1080);
+        Main.mainStage.setScene(Main.scene);
+        Main.mainStage.show();
+    }
 }
