5ab670f9abf4b35d0ac7bea9f78ff9695d170b62
codythai <thaiv@farmingdale.edu>
2025-11-30 22:53:29 -0500

    update firebase and need help

--- src/main/java/farmingdale/collocate/Main.java
+++ src/main/java/farmingdale/collocate/Main.java
@@ -2,6 +2,26 @@ package farmingdale.collocate;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.FirestoreClient;
+
+import com.google.firebase.auth.*;
+import com.google.cloud.firestore.*;
+import com.google.api.core.ApiFuture;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,6 +30,10 @@ import java.io.IOException;
 public class Main extends Application {
     public static Scene scene;
     public static Stage mainStage;
+    public static Firestore fstore;
+    public static FirebaseAuth fauth;
+    private final FirestoreContext contxtFirebase = new FirestoreContext();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
@@ -19,5 +43,17 @@ public class Main extends Application {
         mainStage.setTitle("Collocate");
         mainStage.setScene(scene);
         mainStage.show();
+    }
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,8 @@ module farmingdale.collocate {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+    requires jdk.compiler;
 
     opens farmingdale.collocate to javafx.fxml;
     exports farmingdale.collocate;
