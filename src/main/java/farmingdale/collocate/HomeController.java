efc4b09494b1ecbb332d172b96b5004fa8e6c555
Milton-Moses <miltonmoses05@gmail.com>
2025-12-08 22:15:23 -0500

    init

--- src/main/java/farmingdale/collocate/HomeController.java
+++ src/main/java/farmingdale/collocate/HomeController.java
@@ -1,4 +1,21 @@
 package farmingdale.collocate;
 
+
+import javafx.fxml.FXML;
+
+import javafx.scene.text.Text;
+
 public class HomeController {
+
+    @FXML
+    private Text GreetingText;
+
+    @FXML
+    public void initialize() {
+        TesterPerson myUser = new TesterPerson();
+
+        String fullName = myUser.getUsername();
+        GreetingText.setText("Hello, " + fullName);
+
+    }
 }
